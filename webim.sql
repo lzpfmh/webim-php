a6aaaf35c3d376eebebed20fa4f66bbd90e12ccc
erylee <ery.lee@gmail.com>
2013-08-23 19:01:22 +0800

    fix sql

--- webim.sql
+++ webim.sql
@@ -29,8 +29,8 @@ CREATE TABLE webim_settings(
 	`uid` mediumint(8) unsigned NOT NULL,
 	`web` blob,
 	`air` blob,
-	`created_at` date DEFAULT NULL,
-	`updated_at` date DEFAULT NULL,
+	`created_at` TIMESTAMP DEFAULT NULL,
+	`updated_at` TIMESTAMP DEFAULT NULL,
 	PRIMARY KEY (`id`) 
 )ENGINE=MyISAM;
 
